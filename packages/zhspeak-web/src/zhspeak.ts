4c2e19add72e8da8187ce61de41374dd5d71079f
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-03-23 23:18:31 +0700

    offline-ify

--- packages/zhspeak-web/src/zhspeak.ts
+++ packages/zhspeak-web/src/zhspeak.ts
@@ -4,3 +4,19 @@ const output = document.getElementById('output') as HTMLDivElement
 input.addEventListener('input', () => {
   output.innerText = input.value
 })
+
+window.addEventListener('keydown', (ev) => {
+  if (ev.key === 's') {
+    const s = window.getSelection()!.toString()
+    if (s) {
+      speak(s)
+    }
+  }
+})
+
+function speak (s: string) {
+  const u = new SpeechSynthesisUtterance(s)
+  u.lang = 'zh-CN'
+  u.rate = 0.8
+  speechSynthesis.speak(u)
+}
